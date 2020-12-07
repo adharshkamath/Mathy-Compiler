b67855867ffe6aacd83d6fb519ac32e71356cd0c
Adharsh Kamath <adharshkamathr@gmail.com>
2020-12-07 16:22:07 +0530

    Backend code added

--- app.py
+++ app.py
@@ -1,11 +1,53 @@
 import os
-from flask import Flask
-app = Flask(__name__)
+import sys
+import datetime
+from flask import Flask, request
+from pygments import highlight
+from pygments.lexers import CLexer, CppLexer
+from pygments.formatters import HtmlFormatter
+
+app = Flask("Compiler App Backend")
 
 
-@app.route('/')
-def hello(): 
-    return "Hello World!"
+@app.route("/build")
+def build():
+    if request.method != "POST":
+        return '<h3> Hello! Nothing to see here :/<br><br> Might wanna check <a href="https://github.com/adharshkamath/Mathy-Compiler">this</a> out</h3>'
+    try:
+        os.system("cd system; make")
+        return "Build Succeeded!"
+    except:
+        error = sys.exc_info()[0]
+        print(error)
+        return "Build failed due to " + str(error)
 
-if __name__ == '__main__':
+
+@app.route("/compile", methods=["POST"])
+def compile():
+    if request.method != "POST":
+        return '<h3> Hello! Nothing to see here :/<br><br> Might wanna check <a href="https://github.com/adharshkamath/Mathy-Compiler">this</a> out</h3>'
+    input_code = request.form.get("code")
+    with open("logs.txt", "a") as logs:
+        log_str = "{0} ============= \n {1} \n========================================\n".format(
+            datetime.datetime.now().strftime("%c"), input_code
+        )
+        logs.write(log_str)
+    with open("input.txt", "w") as inputfile:
+        inputfile.write(input_code)
+    os.system("./source/compile input.txt -o ./output")
+    with open("output.c", "r") as outputfile:
+        outputstr = outputfile.read()
+    lexer = CppLexer()
+    formatter = HtmlFormatter(full=True, linenos=True, cssclass="source", style="vs")
+    result = highlight(outputstr, lexer, formatter)
+    return result
+
+
+@app.route("/", defaults={"u_path": ""})
+@app.route("/<path:u_path>")
+def catch_all(u_path):
+    return '<h3> Hello! Nothing to see here :/<br><br> Might wanna check <a href="https://github.com/adharshkamath/Mathy-Compiler">this</a> out</h3>'
+
+
+if __name__ == "__main__":
     app.run()
